10ccc0843cf2815bc92276e85651e20be621ba9f
Leehaney gee <georgeleehaney20@gmail.com>
2023-02-18 19:47:21 +0300

    Feat: Remove nav features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,10 +78,10 @@ const Navbar = () => {
           </div>
         </TooltipComponent>
 
-        {isClicked.cart && (<Cart />)}
+        {/* {isClicked.cart && (<Cart />)}
         {isClicked.chat && (<Chat />)}
         {isClicked.notification && (<Notification />)}
-        {isClicked.userProfile && (<UserProfile />)}
+        {isClicked.userProfile && (<UserProfile />)} */}
       </div>
     </div>
   );
